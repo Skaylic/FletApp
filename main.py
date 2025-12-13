0b9a75f7807ac5ecadba8dfec329f003d63bdb7e
skaylic <skaylic@yandex.ru>
2025-12-13 19:28:17 +1100

    Changes

---
+++ main.py
@@ -0,0 +1,14 @@
+import flet as ft
+from ui.main_app import FletApp
+
+def main(page: ft.Page):
+    # Просто создаем приложение и добавляем на страницу
+    app = FletApp(page)
+    page.add(app)
+
+# Запуск приложения
+ft.app(
+    target=main,
+    # view=ft.AppView.WEB_BROWSER,
+    # port=50000
+)
